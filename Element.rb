00c9ba1f74dd6a38b678515e05f2aa03cf72828e
Grayson Kansala <kansalag@oregonstate.edu>
2021-05-11 20:06:31 -0700

    changed element classes

--- Element.rb
+++ Element.rb
@@ -11,91 +11,79 @@ end
 
 class Rock<Element
   def compare_to(init_type)
-    if init_type.instanceof?(Rock) = Rock 
+    if init_type.instance_of?(Rock) 
   	  return "Rock equals Rock","Tie"
-    elsif init_type.instanceof?(Rock) = Paper
+    elsif init_type.instance_of?(Paper)
 	    return "Rock gets covered by Paper","Lose"
-		elsif init_type.instanceof?(Rock) = Spock
+		elsif init_type.instance_of?(Spock)
 	  	return "Rock gets vaporized by Spock","Lose"
-    elsif init_type.instanceof?(Rock) = Scissors
+    elsif init_type.instance_of?(Scissors)
 		  return "Rock crushes Scissors","Win"
- 		elsif init_type.instanceof?(Rock) = Lizard
+ 		elsif init_type.instance_of?(Lizard) 
   		return "Rock crushes Lizard","Win"
-	  else
-		  fail "This method should be overridden"
     end
   end
 end
 
 class Paper<Element
   def compare_to(init_type)
-    if init_type.instanceof?(Paper) = Paper
+    if init_type.instance_of?(Paper)
 		  return "Paper equals Paper","Tie"
-    elsif Rock == init_type.instanceof?(Paper)
-	 		return "Paper covers Rock","Win"
-    elsif init_type.instanceof?(Paper) = Rock
+    elsif init_type.instance_of?(Rock) 
   		return "Paper covers Rock","Win"
-    elsif init_type.instanceof?(Paper) = Spock
+    elsif init_type.instance_of?(Spock) 
 	  	return "Paper disproves Spock","Win"
-    elsif init_type.instanceof?(Paper) = Scissors
+    elsif init_type.instance_of?(Scissors) 
 	 		return "Paper gets cut by Scissors","Lose" 
-    elsif init_type.instanceof?(Paper) = Lizard
+    elsif init_type.instance_of?(Lizard)
   		return "Paper gets eaten by Lizard","Lose" 
-	 	else
-	  	fail "This method should be overridden"
     end
   end
 end
 
 class Scissors<Element
   def compare_to(init_type)
-    if init_type.instanceof?(Scissors) = Scissors
+    if init_type.instance_of?(Scissors)
       return "Scissors equals Scissors","Tie"
-    elsif init_type.instanceof?(Scissors) = Paper
+    elsif init_type.instance_of?(Paper) 
       return "Scissors cuts Paper","Win" 
-    elsif init_type.instanceof?(Scissors) = Spock
+    elsif init_type.instance_of?(Spock)
       return "Scissors get smashed by Spock","Lose"
-    elsif init_type.instanceof?(Scissors) = Rock
+    elsif init_type.instance_of?(Rock)
       return "Scissors get crushed by Rock","Lose"
-    elsif init_type.instanceof?(Scissors) = Lizard
+    elsif init_type.instance_of?(Lizard)
       return "Scissors decapitate Lizard","Win"
-    else
-      fail "This method should be overridden"
     end
   end
 end
 
 class Lizard<Element
   def compare_to(init_type)
-    if init_type.instanceof?(Lizard) = Paper
+    if init_type.instance_of?(Paper)
 	    return "Lizard eats Paper","Win"
-    elsif init_type.instanceof?(Lizard) = Rock
+    elsif init_type.instance_of?(Rock)
  	    return "Lizard gets crushed by Rock","Lose"
-    elsif init_type.instanceof?(Lizard) = Spock
+    elsif init_type.instance_of?(Spock)
 		  return "Lizard poisons Spock","Win"
-    elsif init_type.instanceof?(Lizard) = Scissors
+    elsif init_type.instance_of?(Scissors)
   	  return "Lizard gets decapitated by Scissors","Lose" 
-	  elsif init_type.instanceof?(Lizard) = Lizard
+	  elsif init_type.instance_of?(Lizard)
  		  return "Lizard equals Lizard","Tie"
-    else
-	    fail "This method should be overridden"
     end
   end
 end
 class Spock<Element
   def compare_to(init_type)
-	  if init_type.instanceof?(Spock) = Paper
+	  if init_type.instance_of?(Paper)
 		  return "Spock gets disproven by Paper","Lose"
-    elsif init_type.instanceof?(Spock) = Rock
+    elsif init_type.instance_of?(Rock)
   		return "Spock vaporizes Rock","Win"
-	  elsif init_type.instanceof?(Spock) = Spock
+	  elsif init_type.instance_of?(Spock)
  			return "Spock equals Spock","Tie"
-    elsif init_type.instanceof?(Spock) = Scissors
+    elsif init_type.instance_of?(Scissors)
   		return "Spock smashes Scissors","Win"
- 		elsif init_type.instanceof?(Spock) = Lizard
+ 		elsif init_type.instance_of?(Lizard)
   		return "Spock gets poison by Lizard","Lose"
-	  else
-		  fail "This method should be overridden"
     end
   end
 end
