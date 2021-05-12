a41ea6e7c236e8a7f6226463fe99faf034770425
Grayson Kansala <kansalag@oregonstate.edu>
2021-05-11 20:16:28 -0700

    Fixed return statements to match

--- Element.rb
+++ Element.rb
@@ -14,9 +14,9 @@ class Rock<Element
     if init_type.instance_of?(Rock) 
   	  return "Rock equals Rock","Tie"
     elsif init_type.instance_of?(Paper)
-	    return "Rock gets covered by Paper","Lose"
+	    return "Paper covers Rock","Lose"
 		elsif init_type.instance_of?(Spock)
-	  	return "Rock gets vaporized by Spock","Lose"
+	  	return "Spock vaporizes Rock","Lose"
     elsif init_type.instance_of?(Scissors)
 		  return "Rock crushes Scissors","Win"
  		elsif init_type.instance_of?(Lizard) 
@@ -34,9 +34,9 @@ class Paper<Element
     elsif init_type.instance_of?(Spock) 
 	  	return "Paper disproves Spock","Win"
     elsif init_type.instance_of?(Scissors) 
-	 		return "Paper gets cut by Scissors","Lose" 
+	 		return "Scissors cut Paper","Lose" 
     elsif init_type.instance_of?(Lizard)
-  		return "Paper gets eaten by Lizard","Lose" 
+  		return "Lizard eats Paper","Lose" 
     end
   end
 end
@@ -48,9 +48,9 @@ class Scissors<Element
     elsif init_type.instance_of?(Paper) 
       return "Scissors cuts Paper","Win" 
     elsif init_type.instance_of?(Spock)
-      return "Scissors get smashed by Spock","Lose"
+      return "Spock smashes Scissors","Lose"
     elsif init_type.instance_of?(Rock)
-      return "Scissors get crushed by Rock","Lose"
+      return "Rock crushes Scissors","Lose"
     elsif init_type.instance_of?(Lizard)
       return "Scissors decapitate Lizard","Win"
     end
@@ -62,11 +62,11 @@ class Lizard<Element
     if init_type.instance_of?(Paper)
 	    return "Lizard eats Paper","Win"
     elsif init_type.instance_of?(Rock)
- 	    return "Lizard gets crushed by Rock","Lose"
+ 	    return "Rock crushes Lizard","Lose"
     elsif init_type.instance_of?(Spock)
 		  return "Lizard poisons Spock","Win"
     elsif init_type.instance_of?(Scissors)
-  	  return "Lizard gets decapitated by Scissors","Lose" 
+  	  return "Scissors decapitate Lizard","Lose" 
 	  elsif init_type.instance_of?(Lizard)
  		  return "Lizard equals Lizard","Tie"
     end
@@ -75,7 +75,7 @@ end
 class Spock<Element
   def compare_to(init_type)
 	  if init_type.instance_of?(Paper)
-		  return "Spock gets disproven by Paper","Lose"
+		  return "Paper disproves Spock","Lose"
     elsif init_type.instance_of?(Rock)
   		return "Spock vaporizes Rock","Win"
 	  elsif init_type.instance_of?(Spock)
@@ -83,13 +83,13 @@ class Spock<Element
     elsif init_type.instance_of?(Scissors)
   		return "Spock smashes Scissors","Win"
  		elsif init_type.instance_of?(Lizard)
-  		return "Spock gets poison by Lizard","Lose"
+  		return "Lizard poisons Spock","Lose"
     end
   end
 end
 
 $MOVES << paper = Paper.new("Paper")
-$MOVES << scissors = Scissor.new("Scissor")
+$MOVES << scissors = Scissors.new("Scissor")
 $MOVES << lizard = Lizard.new("Lizard")
 $MOVES << spock = Spock.new("Spock")
 
